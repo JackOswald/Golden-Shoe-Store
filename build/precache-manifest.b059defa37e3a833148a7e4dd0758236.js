db7e4525f4fca5d2f318cd968afdbab9dcc67865
Jack Oswald <jack_oswald_cfc@hotmail.com>
2019-12-15 00:59:29 +0000

    Initial Commit

---
+++ build/precache-manifest.b059defa37e3a833148a7e4dd0758236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a39be833fba563c9bb427db48b4d81c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1371ba1c5bdf1146730",
+    "url": "/static/css/2.55e53765.chunk.css"
+  },
+  {
+    "revision": "1f0c9136098fc28b0a7e",
+    "url": "/static/css/main.a7756f4a.chunk.css"
+  },
+  {
+    "revision": "c1371ba1c5bdf1146730",
+    "url": "/static/js/2.7ef63d88.chunk.js"
+  },
+  {
+    "revision": "1f0c9136098fc28b0a7e",
+    "url": "/static/js/main.aef95c2b.chunk.js"
+  },
+  {
+    "revision": "d63278640fb4e9cd8ea0",
+    "url": "/static/js/runtime-main.6c056a66.js"
+  }
+]);
